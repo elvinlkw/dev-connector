9b27d525607cf2fb84106564ccbc5d28af45210d
Elvin Li Kam Wa <elvinlkw@hotmail.com>
2021-01-11 22:59:29 -0500

    prepare for deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -11,15 +12,21 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API LOADED');
-})
-
 // Define Routes
 const usersRoute = require('./routes/api/users');
 const authRoute = require('./routes/api/auth');
 const profileRoute = require('./routes/api/profile');
 const postsRoute = require('./routes/api/posts');
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  })
+}
 
 app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoute);
